b93fb6e393fabfd10c8a7f960a2f1d795638e82e
jabbyy <jeppem65@gmail.com>
2023-02-28 08:28:00 +0100

    Initial commit

---
+++ server-prog-blazer-app/server-prog-blazer-app/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace server_prog_blazer_app.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
